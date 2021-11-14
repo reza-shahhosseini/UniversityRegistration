5fd58303501189645cb6a2a4069708a59843ecd5
Reza Shah Hosseini <rezashahhosseini@outlook.com>
2021-11-14 23:41:04 +0330

    Second Commit - Client Side Skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
